3dcbe9f5782dcd41ed99c5877c0ad7fc9a125485
Matt Fisher <matt.fisher@nsidc.org>
2022-07-05 18:46:49 -0600

    Hello, world!

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/index.css';
+
+// TODO: Do we need all these weights?
+import '@fontsource/roboto';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+		<div>
+			<p>Hello, world!</p>
+		</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
