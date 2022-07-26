8f2583a33f1e5861016f14b8cfa7851fa0f74134
Matt Fisher <matt.fisher@nsidc.org>
2022-07-26 00:21:13 +0000

    Display debug button conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,19 @@ import SlippyMap from './components/SlippyMap';
 import BasemapSelector from './components/BasemapSelector';
 import DebugDumpButton from './components/DebugDumpButton';
 
+declare var __ENVIRONMENT__: 'development' | 'production';
+if (__ENVIRONMENT__ === 'development') {
+  console.log(`Environment is: ${__ENVIRONMENT__}`);
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div id="version">v{pkg.version}</div>
 
-      <DebugDumpButton />
+      {__ENVIRONMENT__ === 'development' &&
+        <DebugDumpButton />
+      }
 
       <SlippyMap />
 
