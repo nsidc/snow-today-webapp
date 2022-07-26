19cee639eef03bd930a1f1d6af907ef0dee4c2e8
Matt Fisher <matt.fisher@nsidc.org>
2022-07-26 00:14:05 +0000

    Fix issues with debug button

--- src/components/DebugDumpButton.tsx
+++ src/components/DebugDumpButton.tsx
@@ -5,12 +5,14 @@ import {useRecoilCallback} from 'recoil';
 const DebugDumpButton: React.FC = () => {
   const onClick = useRecoilCallback(({snapshot}) => async () => {
     console.debug('Atom values:');
-    Array.from(snapshot.getNodes_UNSTABLE()).forEach(async (node) => {
+
+    for (const node of snapshot.getNodes_UNSTABLE()) {
       const value = await snapshot.getPromise(node);
       console.debug(node.key, value);
-    });
+    }
   }, []);
 
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return <button onClick={onClick}>Dump State</button>
 }
 
