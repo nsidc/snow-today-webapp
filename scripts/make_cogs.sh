f57515b4c015130b58f8de1ee7adc1f1cfb85122
Matt Fisher <matt.fisher@nsidc.org>
2022-07-28 00:57:31 +0000

    Comments for COG script

--- scripts/make_cogs.sh
+++ scripts/make_cogs.sh
@@ -16,7 +16,15 @@ for file in originals/*; do
     wip_file="${output_dir}/wip_$(basename $file)";
     of="${output_dir}/$(basename $file)";
 
+    # Reproject to Web Mercator.
+    # NOTE: This is not optimal, as the input files are in an intermediate
+    # projection (EPSG:4326), not their original projection. By reprojecting
+    # twice, we introduce error.
     gdalwarp -t_srs "EPSG:3857" "$file" "$wip_file";
+
+    # Make it Cloud-Optimized
     gdal_translate "$wip_file" "$of" -of COG -co COMPRESS=LZW;
+
+    # Cleanup intermediate files
     rm $wip_file;
 done
