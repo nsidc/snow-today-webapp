bd4bdab8085f242e36bfb7d21694f54d5b0b2dd8
Matt Fisher <matt.fisher@nsidc.org>
2022-08-02 20:48:23 +0000

    Index the shape data

--- src/constants/dataServer.ts
+++ src/constants/dataServer.ts
@@ -1,3 +1,11 @@
+// TODO: Should we just develop an API that serves the correct TIF or GeoJSON
+// (or other data)?
 export const dataServerUrl = 'http://integration.snow-today.apps.int.nsidc.org:8000'
 
-export const testDataUrl = `${dataServerUrl}/cogs/SnowToday_USwest_20220629_Albedo.tif`;
+// Cloud-optimized GeoTIFFs:
+export const cogsServerUrl = `${dataServerUrl}/cogs`;
+export const testDataUrl = `${cogsServerUrl}/SnowToday_USwest_20220629_Albedo.tif`;
+
+// Information about regions:
+export const shapesServerUrl = `${dataServerUrl}/shapes`;
+export const stateShapeIndexJson = `${shapesServerUrl}/states/index.json`;
