819f91196e6bb14d448e7b8cc10363887fef5fd6
Matt Fisher <matt.fisher@nsidc.org>
2022-08-03 23:56:17 +0000

    Use un-dated test GeoTIFF

--- src/constants/dataServer.ts
+++ src/constants/dataServer.ts
@@ -1,3 +1,3 @@
 export const dataServerUrl = 'http://integration.snow-today.apps.int.nsidc.org:8000'
 
-export const testDataUrl = `${dataServerUrl}/cogs/SnowToday_USwest_20220629_Albedo.tif`;
+export const testDataUrl = `${dataServerUrl}/cogs/SnowToday_USwest_Albedo.tif`;
