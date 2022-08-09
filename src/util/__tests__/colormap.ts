013d032fccc92adb4389d2b88e86d32078c51b4f
Matt Fisher <matt.fisher@nsidc.org>
2022-08-09 20:28:26 +0000

    Fix test with missing colormap data

--- src/util/__tests__/colormap.ts
+++ src/util/__tests__/colormap.ts
@@ -1,13 +1,24 @@
-import {colormapBuPu} from '../../constants/colormaps';
 import {
   colorStopsFromColorMap,
   colorStopsFromColorMapName,
 } from '../colormap'
 
+const colormapTest = [
+  [255, 255, 217],
+  [237, 248, 177],
+  [199, 233, 180],
+  [127, 205, 187],
+  [65, 182, 196],
+  [29, 145, 192],
+  [34, 94, 168],
+  [37, 52, 148],
+  [8, 29, 88],
+]
+
 
 test('Generates correct number of color stops from hard-coded colormap', () =>{
-  const colorStops = colorStopsFromColorMap(colormapBuPu, 0, 100, false);
-  expect(colorStops.length).toEqual(colormapBuPu.length * 2);
+  const colorStops = colorStopsFromColorMap(colormapTest, 0, 100, false);
+  expect(colorStops.length).toEqual(colormapTest.length * 2);
 });
 
 test('Generates correct number of color stops from named colormap', () => {
