8088439054d2d3ad8f84ddc11cf5f0e431c5dd8c
Matt Fisher <matt.fisher@nsidc.org>
2022-09-06 17:58:59 +0000

    Flip-flop back to QA data server URL for now
    We can return to prod URL when the webapp itself is in prod! Is there a
    way we can change this URL depending on where we deploy? ¯\_(ツ)_/¯

--- src/constants/dataServer.ts
+++ src/constants/dataServer.ts
@@ -1,4 +1,5 @@
-export const dataServerUrl = 'https://nsidc.org/api/snow-today'
+// export const dataServerUrl = 'https://nsidc.org/api/snow-today'
+export const dataServerUrl = 'https://qa.nsidc.org/api/snow-today'
 
 // Information about regions:
 export const regionsIndexUrl = `${dataServerUrl}/regions.json`;
