87d7ba9da4af82534b330239504fbfe64314508a
Matt Fisher <matt.fisher@nsidc.org>
2023-10-17 14:22:33 -0600

    Add error boundary with non-working "retry" button
    Catches errors, but offers only a message and false hope

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import pkg from '../package.json';
 import './style/App.css';
 import ControlPanel from './components/ControlPanel';
 import TileLayout from './components/TileLayout';
 
+
+const ErrorFallbackComponent = ({ error, resetErrorBoundary }) => (
+  <div role="alert">
+    <p>Something no good happen!</p>
+    <pre style={{backgroundColor: "whitesmoke", color: "red"}}>{error.message}</pre>
+
+    <button onClick={resetErrorBoundary}>Retry</button>
+  </div>
+);
+
+
 const App: React.FC = () => {
   return (
     <div className={"App"}>
       <div id={"version"}>v{pkg.version}</div>
 
-      <ControlPanel />
-      <TileLayout />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallbackComponent}
+        onReset={(something) => {
+          console.log(something);
+        }}
+      >
+        <ControlPanel />
+        <TileLayout />
+      </ErrorBoundary>
 
     </div>
   );
