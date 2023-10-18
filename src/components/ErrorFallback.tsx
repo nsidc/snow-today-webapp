a7591808daad5ce83ac05c390e98992cd9262538
Ann Windnagel <115041013+windnagelnsidc@users.noreply.github.com>
2023-10-18 17:24:02 -0600

    Update ErrorFallback.tsx

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -7,13 +7,11 @@ const ErrorFallbackComponent = ({error, resetErrorBoundary}: ErrorFallbackParams
 
     <p>
      Something went wrong. This application is currently unavailable. Please try again later.
-     If the error persists, copy the red error text and send to NSIDC User Services at the link below.
     </p>
 
     <p><b>
-      {"Please contact "}
-      <a href="mailto:nsidc@nsidc.org">NSIDC User Services</a>
-      {" if you need additional assistance."}
+      {"If the error persists, copy the red error text and send to "}
+      <a href="mailto:nsidc@nsidc.org">NSIDC User Services</a>.
     </b></p>
   </div>
 );
