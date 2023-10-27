3abbca746d30df80a07e38c9963c0a2425dc3c0f
Matt Fisher <matt.fisher@nsidc.org>
2023-10-27 15:40:29 -0600

    Make superRegionId a required arg for subregion query

--- src/serverState/regionsIndex.ts
+++ src/serverState/regionsIndex.ts
@@ -1,14 +1,13 @@
 import {useQuery} from '@tanstack/react-query';
 
-import {fetchSuperRegionsIndex} from '@src/util/fetch/regions';
-import {ISuperRegionIndex} from '@src/types/query/regions';
+import {fetchSuperRegionsIndex, fetchSubRegionsIndex} from '@src/util/fetch/regions';
+import {ISuperRegionIndex, ISubRegionIndex} from '@src/types/query/regions';
 
 
-export const SERVERSTATE_KEY_REGIONS_INDEX = 'superRegionsIndex';
-
-const useRegionsIndexQuery = () => {
+export const SERVERSTATE_KEY_SUPERREGIONS_INDEX = 'superRegionsIndex';
+export const useSuperRegionsIndexQuery = () => {
   return useQuery<ISuperRegionIndex>(
-    [SERVERSTATE_KEY_REGIONS_INDEX],
+    [SERVERSTATE_KEY_SUPERREGIONS_INDEX],
     fetchSuperRegionsIndex,
     {
       // Never re-fetch this data!
@@ -19,4 +18,23 @@ const useRegionsIndexQuery = () => {
     }
   );
 }
-export default useRegionsIndexQuery;
+
+export const SERVERSTATE_KEY_SUBREGIONS_INDEX = 'subRegionsIndex';
+export const useSubRegionsIndexQuery = (
+  superRegionId: string,
+) => {
+  return useQuery<ISubRegionIndex>(
+    [SERVERSTATE_KEY_SUBREGIONS_INDEX, superRegionId],
+    () => {
+      return fetchSubRegionsIndex(superRegionId);
+    },
+    {
+      enabled: !!superRegionId,
+      // Never re-fetch this data!
+      cacheTime: Infinity,
+      staleTime: Infinity,
+      // Propagate all errors to the nearest error boundary
+      useErrorBoundary: true,
+    }
+  );
+}
