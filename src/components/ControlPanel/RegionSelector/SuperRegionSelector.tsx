8fb754c39329e78d45f196bed063b8f395c728ed
Matt Fisher <matt.fisher@nsidc.org>
2023-11-10 18:14:33 -0700

    Add questionable state change behavior matching splash screen

--- src/components/ControlPanel/RegionSelector/SuperRegionSelector.tsx
+++ src/components/ControlPanel/RegionSelector/SuperRegionSelector.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useSetRecoilState} from 'recoil';
 
 import '@src/style/SuperRegionSelector.css';
 import selectedSuperRegionIdAtom from '@src/state/client/selectedSuperRegionId';
+import selectedRegionIdAtom from '@src/state/client/selectedRegionId';
 import {ISuperRegionIndex} from '@src/types/query/regions';
 
 
@@ -12,6 +13,7 @@ interface ISuperRegionSelectorProps {
 
 const SuperRegionSelector: React.FC<ISuperRegionSelectorProps> = (props) => {
   const [selectedSuperRegionId, setSelectedSuperRegionId] = useRecoilState(selectedSuperRegionIdAtom);
+  const setSelectedRegionId = useSetRecoilState(selectedRegionIdAtom);
 
   const superRegionOptions = Object.entries(props.indexQueryData).map(
     ([superRegionId, superRegion]) => (
@@ -21,12 +23,20 @@ const SuperRegionSelector: React.FC<ISuperRegionSelectorProps> = (props) => {
 
   /* TODO: Handle undefined super region? Let it fall to an error boundary? */
 
+  const handleSelection = (e) => {
+    const newRegionId = e.currentTarget.value;
+    setSelectedSuperRegionId(newRegionId);
+    // TODO: Is this a good practice? Setting two pieces of Recoil state
+    // together? Why not use the state graph?
+    setSelectedRegionId(newRegionId);
+  }
+
   return (
     <div id={'SuperRegionSelector'}>
       <label htmlFor={'super-region-selector'}>{'Region: '}</label>
       <select id={'super-region-selector'}
         value={selectedSuperRegionId}
-        onChange={e => setSelectedSuperRegionId(e.currentTarget.value)}
+        onChange={handleSelection}
       >
         {superRegionOptions}
       </select>
