5bd2a4969446c55a9629e0eade3002e307b71b8b
Matt Fisher <matt.fisher@nsidc.org>
2023-11-14 08:53:24 -0700

    Eject config from eslint-config-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
+  "root": true,
+
   "parser": "@typescript-eslint/parser",
   // The below config enables type-aware linting, but significantly slows down
   // eslint.
@@ -7,20 +9,35 @@ module.exports = {
     "tsconfigRootDir": __dirname,
     "project": ["./tsconfig.json"],
   },
+
   "plugins": [
     "@typescript-eslint",
+    "react",
+    "import",
+    "flowtype",
+    "jsx-a11y",
     "react-hooks",
+    "jest",
+    "testing-library",
   ],
+
   "extends": [
-    "react-app",
-    "react-app/jest",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
+
+  "settings": {
+    "react": {
+      "version": "detect",
+    },
+  },
+
   "rules": {
     "eqeqeq": [
       "error",
     ],
+
+    // react
     "react/function-component-definition": [
       "error",
       {
@@ -30,6 +47,8 @@ module.exports = {
     ],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+
+    // typescript
     "@typescript-eslint/ban-ts-comment": [
       "warn",
       {
@@ -38,6 +57,90 @@ module.exports = {
     ],
     "@typescript-eslint/no-explicit-any": [
       "warn",
-    ]
+    ],
+
+    // migrated from:
+    // https://github.com/facebook/create-react-app/blob/0a827f69ab0d2ee3871ba9b71350031d8a81b7ae/packages/eslint-config-react-app/base.js
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-uses-react': 'warn',
+
+    // migrated from:
+    // https://github.com/facebook/create-react-app/blob/0a827f69ab0d2ee3871ba9b71350031d8a81b7ae/packages/eslint-config-react-app/index.js
+    /// TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+    'default-case': 'off',
+    /// 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+    'no-dupe-class-members': 'off',
+    /// 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+    'no-undef': 'off',
+
+    /// Add TypeScript specific rules (and turn off ESLint equivalents)
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': 'warn',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'warn',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false,
+      },
+    ],
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'warn',
+
+    // migrated from:
+    // https://github.com/facebook/create-react-app/blob/0a827f69ab0d2ee3871ba9b71350031d8a81b7ae/packages/eslint-config-react-app/jest.js
+    /// https://github.com/jest-community/eslint-plugin-jest
+    'jest/no-conditional-expect': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/no-interpolation-in-snapshots': 'error',
+    'jest/no-jasmine-globals': 'error',
+    'jest/no-mocks-import': 'error',
+    'jest/valid-describe-callback': 'error',
+    'jest/valid-expect': 'error',
+    'jest/valid-expect-in-promise': 'error',
+    'jest/valid-title': 'warn',
+
+    /// https://github.com/testing-library/eslint-plugin-testing-library
+    'testing-library/await-async-queries': 'error',
+    'testing-library/await-async-utils': 'error',
+    'testing-library/no-await-sync-queries': 'error',
+    'testing-library/no-container': 'error',
+    'testing-library/no-debugging-utils': 'error',
+    'testing-library/no-dom-import': ['error', 'react'],
+    'testing-library/no-node-access': 'error',
+    'testing-library/no-promise-in-fire-event': 'error',
+    'testing-library/no-render-in-lifecycle': 'error',
+    'testing-library/no-unnecessary-act': 'error',
+    'testing-library/no-wait-for-multiple-assertions': 'error',
+    'testing-library/no-wait-for-side-effects': 'error',
+    'testing-library/no-wait-for-snapshot': 'error',
+    'testing-library/prefer-find-by': 'error',
+    'testing-library/prefer-presence-queries': 'error',
+    'testing-library/prefer-query-by-disappearance': 'error',
+    'testing-library/prefer-screen-queries': 'error',
+    'testing-library/render-result-naming-convention': 'error',
+    // end migrated config
   },
 }
