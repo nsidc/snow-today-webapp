317405d4f69c0a17c16bce70b4cfcb361177f5f7
Matt Fisher <matt.fisher@nsidc.org>
2024-03-06 10:47:35 -0700

    Use USGS topographic imagery basemap by default
    Improve type annotations while I'm in there

--- src/state/client/selectedBasemapName.ts
+++ src/state/client/selectedBasemapName.ts
@@ -1,6 +1,7 @@
 import {atom} from 'jotai';
 
+import {BasemapTitle} from '@src/util/layer/basemaps';
 
 // TODO: Something about this magic string
-export const selectedBasemapNameAtom = atom<string>('USGS Topographic');
+export const selectedBasemapNameAtom = atom<BasemapTitle>('USGS Topographic + Imagery');
 selectedBasemapNameAtom.debugLabel = 'selectedBasemapNameAtom';
